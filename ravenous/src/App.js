f8edb6cff604e6d8566319f2607a37a686b8ce93
Neel Bhattacharyya <neelbh@umich.edu>
2019-04-09 19:32:00 -0400

    Add files via upload

---
+++ ravenous/src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import BusinessList from './Components/BusinessList/BusinessList';
+import SearchBar from './Components/SearchBar/SearchBar';
+import yelp from './util/yelp';
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      businesses: []
+    };
+
+    this.searchYelp = this.searchYelp.bind(this);
+  }
+  
+  searchYelp(term, location, sortBy) {
+    yelp.search(term, location, sortBy).then(businesses => {
+      this.setState({businesses: businesses});
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar searchYelp={this.searchYelp} />
+        <BusinessList businesses={this.state.businesses} />
+      </div>
+    );
+  }
+}
+
+export default App;
